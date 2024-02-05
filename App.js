2d85c708c0ac1326fa066ff02896fb6618c4a880
Shashi7427 <shashitarode1@gmail.com>
2024-02-05 23:34:37 +0530

    added ask page and homescreen page in the app

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
-import React from 'react'
-import { Provider } from 'react-native-paper'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import { theme } from './src/core/theme'
+import React from "react";
+import { Provider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { theme } from "./src/core/theme";
 import {
   LoginScreen,
   RegisterScreen,
   ResetPasswordScreen,
   Dashboard,
+} from "./src/screens";
+import Details from "./src/components/Details";
+import Ask from "./src/components/Ask";
+import Homescreen from "./src/components/Homescreen";
 
-} from './src/screens'
-
-const Stack = createStackNavigator()
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
@@ -26,6 +28,9 @@ export default function App() {
           <Stack.Screen name="LoginScreen" component={LoginScreen} />
           <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
           <Stack.Screen name="Dashboard" component={Dashboard} />
+          <Stack.Screen name="Details" component={Details} />
+          <Stack.Screen name="Ask" component={Ask} />
+          <Stack.Screen name="HomeScreen" component={Homescreen} />
           <Stack.Screen
             name="ResetPasswordScreen"
             component={ResetPasswordScreen}
@@ -33,5 +38,5 @@ export default function App() {
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
-  )
+  );
 }
